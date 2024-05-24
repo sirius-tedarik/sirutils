cfc8e5606eb5ec4b3513254a850968c3c2472601
giveerr <givessomeerr.e@gmail.com>
2024-05-24 16:24:57 +0300

    refactor(@sirutils/schema): uncomment cache strategy

--- experiments/db/src/index.ts
+++ experiments/db/src/index.ts
@@ -17,19 +17,3 @@ if (data.isErr()) {
   // biome-ignore lint/nursery/noConsole: <explanation>
   console.error(data.error.data)
 }
-
-/*
-wrap((data: ${_.upperFirst(file.name)}) => {
-  const result = compiled.Check(data)
-
-  if (!result) {
-    unwrap(
-      ProjectError.create(schemaTags.invalidData, 'invalid data')
-        .appendData([...compiled.Errors(data)])
-        .asResult()
-    )
-  }
-
-  return true as const
-}, schemaTags.invalidData)
- */
