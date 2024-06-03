fc69be2d3e486a56ce2c0b9b999c51f06de5b686
giveerr <givessomeerr.e@gmail.com>
2024-06-03 10:10:04 +0300

    refactor(@sirutils/{toolbox,schema}): rename toolbox error key and remove fields file in schema

--- tools/schema/src/index.ts
+++ tools/schema/src/index.ts
@@ -1,8 +1,16 @@
 /// <reference types="@sirutils/seql" />
 /// <reference types="@sirutils/core" />
+/// <reference types="@sirutils/toolbox" />
+
 import './definitions'
 
 export * from './tag'
 
 export * from './utils/traverse'
 export * from './utils/plugin'
+export * from './utils/normalize'
+export * from './utils/json-schema'
+
+export * from './utils/generator/imports'
+export * from './utils/generator/definitions'
+export * from './utils/generator/checksum'
