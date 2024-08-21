8d208599b9606e50ff9a0559f79af49aa0847cf3
giveerr <givessomeerr.e@gmail.com>
2024-08-21 15:02:27 +0300

    chore(@sirutils/docs): fix for render

--- apps/docs/nuxt.config.ts
+++ apps/docs/nuxt.config.ts
@@ -2,4 +2,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   extends: ['shadcn-docs-nuxt'],
   compatibilityDate: '2024-07-06',
+  devServer: {
+    port: 10_000,
+    host: '0.0.0.0',
+  },
 })
