288dc0a36e4e56f73b1b4bcaa703bc335d5cc27d
giveerr <givessomeerr.e@gmail.com>
2024-08-21 15:50:03 +0300

    chore(@sirutils/docs): fix typescript config

--- apps/docs/nuxt.config.ts
+++ apps/docs/nuxt.config.ts
@@ -2,8 +2,4 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   extends: ['shadcn-docs-nuxt'],
   compatibilityDate: '2024-07-06',
-  devServer: {
-    port: 10_000,
-    host: '0.0.0.0',
-  },
 })
